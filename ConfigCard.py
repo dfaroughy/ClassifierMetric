b7314f7598c7af041add355594466496bf077c82
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-07-19 16:00:33 +0200

    restructured configs

--- ConfigCard.py
+++ ConfigCard.py
@@ -1,18 +1,16 @@
 class DataConfig:
 
     jet_type : str = 'tops'
-
-    sets : dict = {'flow-match' : ('generated_data_FM.npy', None),
-                   'diffusion'  : ('ddim_200.h5', 'etaphipt_frac'),
-                   'jetnet'     : ('t150.hdf5', 'particle_features')}
-
     preprocess: dict = {'normalize': {},
                         'logit_transform': {'alpha' : 1e-5},
                         'standardize': {'sigma' : 1.0}}
-  
 class TrainConfig:
 
-    size : int = 175000
+    datafiles : dict = {'fm_midpoint' : ('fm_tops150_mp200nfe.h5', 'etaphipt'),
+                        'fm_euler'    : ('fm_tops150_eu200nfe.h5', 'etaphipt'),
+                        'diff_ddim'   : ('ddim_200.h5', 'etaphipt_frac')}
+
+    size : int = None
     test_size : float = 0.7    
     batch_size : int = 1024
     epochs : int = 10000   
@@ -21,6 +19,8 @@ class TrainConfig:
 
 class EvalConfig:
 
-    size : int = 50000  
+    datafiles : dict = {'jetnet' : ('t150.hdf5', 'particle_features')}
+    size : int = None  
+    test_size : float = 0.7
     batch_size : int = 1024 
     class_label : int = 0
