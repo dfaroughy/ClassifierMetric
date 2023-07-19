7fe07caa8404c7d5d34438af8159a9b586b715f7
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-07-19 18:29:47 +0200

    added class labels to JetNetLoader

--- src/dataloader.py
+++ src/dataloader.py
@@ -11,6 +11,9 @@ class JetNetDataLoader(Dataset):
 
     ''' Arguments:
         - `dir_path` : path to data files
+        - `data_files` : dictionary of data files to load, default is `None`
+        - `data_class_labels` : dictionary of class labels for each data file, default is `None`
+        - `num_jets`: number of jets to load, default is `None`
         - `num_constituents`: number of particle constituents in each jet, default is `150`
         - `particle_features`: list of particle features to include in data, default is `['eta_rel', 'phi_rel', 'pt_rel']`
         - `preprocess`: dictionary of preprocessing methods to apply to data, default is `None`
@@ -18,6 +21,7 @@ class JetNetDataLoader(Dataset):
     
         Loads and formats data from data files in format `.npy` or `.hdf5`.\\
         Adds mask and zero-padding if necessary.\\
+        Adds class label for each sample if `data_class_labels` is provided.\\
         Input data in files should always have shape (`#jets`, `#particles`, `#features`) with feaures: `(eta_rel, phi_rel, pt_rel, ...)`
         
     '''
@@ -25,6 +29,7 @@ class JetNetDataLoader(Dataset):
     def __init__(self, 
                  dir_path: str=None, 
                  data_files: dict=None,
+                 data_class_labels: dict=None,
                  num_jets: int=None,
                  num_constituents: int=150,
                  particle_features: list=['eta_rel', 'phi_rel', 'pt_rel'],
@@ -33,6 +38,7 @@ class JetNetDataLoader(Dataset):
         
         self.path = dir_path
         self.data_files = data_files
+        self.data_class_labels = data_class_labels
         self.num_jets = num_jets
         self.num_consts = num_constituents
         self.particle_features = particle_features
@@ -44,19 +50,18 @@ class JetNetDataLoader(Dataset):
         return len(self.data_list)
     
     def __getitem__(self, idx):
-        name, filename, key = self.data_list[idx]
+        name, filename, key, label = self.data_list[idx]
         data = torch.Tensor(self.load_file(filename, key))
 
         N = data.shape[0]
 
         if len(data.shape) == 3:
 
-            _, P, D = data.shape
+            N, P, D = data.shape
             
             #...fill with zero padding up to 'num_constituents'
 
             if D < 3: raise ValueError('Data has wrong shape: {}'.format(data.shape))
-            if P > self.num_consts: raise ValueError('Data has wrong shape: {}'.format(data.shape))
             elif P < self.num_consts: 
                 zero_rows = torch.zeros(N, self.num_consts - P, D)
                 data = torch.cat((data, zero_rows), dim=1) 
@@ -93,7 +98,17 @@ class JetNetDataLoader(Dataset):
                 _data.preprocess(methods=self.preprocess, name=name)
                 data = _data.particles
 
-        return data[:self.num_jets if self.num_jets is not None else N]
+            #...remove sofetst jet constituents if more than 'num_constituents'
+
+            if P > self.num_consts:
+                data = data[:, :self.num_consts, :]
+
+        if label is None: 
+            return data[:self.num_jets if self.num_jets is not None else N]
+        else:
+            data = data[:self.num_jets if self.num_jets is not None else N] 
+            labels = torch.full_like(data[..., 0], label).unsqueeze(-1)
+            return data, labels
     
     def pt_order(self, data):
         _ , i = torch.sort(torch.abs(data[:, :, 2]), dim=1, descending=True) 
@@ -122,15 +137,19 @@ class JetNetDataLoader(Dataset):
                 key = None
                 path = os.path.join(root, file)
 
+                # numpy files
                 if file.endswith('.npy'):
                     i+=1
                     if self.data_files is not None:
                         for k in self.data_files.keys():
                             if file in self.data_files[k][0] and key==self.data_files[k][1]:
-                                data_list.append((k, file, key))  
+                                label = None if self.data_class_labels is None else self.data_class_labels[k]
+                                data_list.append((k, file, key, label))  
                     else:
-                        data_list.append(('sample_{}'.format(i), file, key))   
+                        label = None if self.data_class_labels is None else i
+                        data_list.append(('sample_{}'.format(i), file, key, label))   
 
+                # hdf5 files
                 elif file.endswith('.hdf5'):
                     with h5py.File(path, 'r') as f:
                         for key in f.keys():
@@ -138,9 +157,11 @@ class JetNetDataLoader(Dataset):
                             if self.data_files is not None:
                                 for k in self.data_files.keys():
                                     if file in self.data_files[k][0] and key==self.data_files[k][1]:
-                                        data_list.append((k, file, key))
+                                        label = None if self.data_class_labels is None else self.data_class_labels[k]
+                                        data_list.append((k, file, key, label))
                             else:
-                                data_list.append(('sample_{}'.format(i), file, key))
+                                label = None if self.data_class_labels is None else i
+                                data_list.append(('sample_{}'.format(i), file, key, label))
 
                 elif file.endswith('.h5'):
                     with h5py.File(path, 'r') as f:
@@ -149,21 +170,29 @@ class JetNetDataLoader(Dataset):
                             if self.data_files is not None:
                                 for k in self.data_files.keys():
                                     if file in self.data_files[k][0] and key==self.data_files[k][1]:
-                                        data_list.append((k, file, key))
+                                        label = None if self.data_class_labels is None else self.data_class_labels[k]
+                                        data_list.append((k, file, key, label))
                             else:
-                                data_list.append(('sample_{}'.format(i), file, key))
+                                label = None if self.data_class_labels is None else i
+                                data_list.append(('sample_{}'.format(i), file, key, label))
         return data_list
     
     def data_summary(self):
         table = []
-        headers = ['Sample Name', 'Filename', 'Extension', 'Key', 'Shape']
-        for name, filename, key in self.data_list:
+        headers = ['Sample Name', 'Filename', 'Extension', 'Key', 'Shape', 'Class Label']
+        # for name, filename, key, label in self.data_list:
+        #     filename, ext = os.path.splitext(filename)
+        #     shape = self.load_file(filename, key).shape
+        #     table.append([name, filename, ext, key, shape, label])
+        # print(tabulate(table, headers=headers))
+
+        for name, filename, key, label in self.data_list:
             data = self.load_file(filename, key)
             filename, ext = os.path.splitext(filename)
             filename = os.path.basename(filename)
-            table.append([name, filename, ext, key, str(data.shape)])
-        print(tabulate(table, headers=headers, tablefmt='pretty'))  
+            table.append([name, filename, ext, key, str(data.shape), label])
 
+        print(tabulate(table, headers=headers, tablefmt='pretty'))  
 
 
 
