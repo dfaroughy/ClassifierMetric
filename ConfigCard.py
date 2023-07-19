9ac5ea06d615a83c983d5626128138fadc2d8664
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-07-19 15:09:34 +0200

    restructurd JetNetFeatures, JetNetDataLoader

--- ConfigCard.py
+++ ConfigCard.py
@@ -2,21 +2,17 @@ class DataConfig:
 
     jet_type : str = 'tops'
 
-    sets : dict = {
-                   'flow-match' : ('generated_data_FM.npy', None),
+    sets : dict = {'flow-match' : ('generated_data_FM.npy', None),
                    'diffusion'  : ('ddim_200.h5', 'etaphipt_frac'),
-                   'jetnet'     : ('t150.hdf5', 'particle_features')
-                   }
+                   'jetnet'     : ('t150.hdf5', 'particle_features')}
 
-    preprocess: dict = {
-                        'normalize': {},
+    preprocess: dict = {'normalize': {},
                         'logit_transform': {'alpha' : 1e-5},
-                        'standardize': {'sigma' : 1.0}
-                        }
+                        'standardize': {'sigma' : 1.0}}
   
 class TrainConfig:
 
-    size : int = 150000
+    size : int = 175000
     test_size : float = 0.7    
     batch_size : int = 1024
     epochs : int = 10000   
