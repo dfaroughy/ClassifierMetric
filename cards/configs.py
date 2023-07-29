5df6d3aa06d18004593e836ae13c69739f8b928a
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-07-29 12:05:07 +0200

    update

--- cards/configs.py
+++ cards/configs.py
@@ -2,13 +2,21 @@ class DataConfig:
 
     jet_type = 'tops'
 
-    datasets = {'test'     : ['t150.hdf5', 'particle_features'],
-                'flow_mid'  : ['fm_tops150_cond_mp200nfe.h5', 'etaphipt'],
-                'diff_mid'  : ['diff_midpoint_100_csts.h5', 'etaphipt_frac']}
+    datasets = {
+                'flow_midpoint'  : ['fm_tops150_cond_mp200nfe.h5', 'etaphipt'],
+                'diff_midpoint'  : ['midpoint_100_csts.h5', 'etaphipt_frac'],
+                'flow_euler'     : ['fm_tops150_cond_eu200nfe.h5', 'etaphipt'],
+                'diff_euler'     : ['euler_200_csts.h5', 'etaphipt_frac'],
+                'jetnet150'      : ['t150.hdf5', 'particle_features']
+                }
     
-    labels = {'flow_mid'   : 0,
-              'diff_mid'   : 1,
-              'test'      : -1} 
+    labels = {
+              'flow_midpoint'   : 0,
+              'diff_midpoint'   : 1,
+              'flow_euler'      : 2,
+              'diff_euler'      : 3,
+              'jetnet150'       : -1  # test dataset
+              } 
     
     features = ['eta_rel', 'phi_rel', 'pt_rel', 'R', 'e_rel']
     preprocess = ['standardize']
@@ -16,8 +24,8 @@ class DataConfig:
 class TrainConfig:
 
     device = 'cpu'
-    split_fractions = [0.4, 0.3, 0.3]  # train / val / test 
-    size = 220000 
+    split_fractions = [0.5, 0.2, 0.3]  # train / val / test 
+    size = None 
     batch_size  = 1024
     epochs = 10000   
     early_stopping  = 20 
