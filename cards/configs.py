5ca9e6be1a2295d52359c1d642059052ae0caed5
Darius Faroughy <dariusfar@gmail.com>
2023-07-31 10:46:20 -0400

    update

--- cards/configs.py
+++ cards/configs.py
@@ -6,21 +6,21 @@ class DataConfig:
     datasets = {
                 'flow_midpoint'  : ['fm_tops150_cond_mp200nfe.h5', 'etaphipt'],
                 'diff_midpoint'  : ['midpoint_100_csts.h5', 'etaphipt_frac'],
-                'flow_euler'     : ['fm_tops150_cond_eu200nfe.h5', 'etaphipt'],
-                'diff_euler'     : ['euler_200_csts.h5', 'etaphipt_frac'],
+                # 'flow_euler'     : ['fm_tops150_cond_eu200nfe.h5', 'etaphipt'],
+                # 'diff_euler'     : ['euler_200_csts.h5', 'etaphipt_frac'],
                 'jetnet150'      : ['t150.hdf5', 'particle_features']
                 }
     labels = {
               'flow_midpoint'   : 0,
               'diff_midpoint'   : 1,
-              'flow_euler'      : 2,
-              'diff_euler'      : 3,
+            #   'flow_euler'      : 2,
+            #   'diff_euler'      : 3,
               'jetnet150'       : -1  # test dataset
               } 
     
 class TrainConfig:
 
-    device = 'cuda:3'
+    device = 'cuda:1'
     split_fractions = [0.5, 0.2, 0.3]  # train / val / test 
     size = None 
     batch_size  = 1024
