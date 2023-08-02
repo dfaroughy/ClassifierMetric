876ec1e352e8d4ac7f62b2c1ec13cbf2588ac4b4
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-08-02 12:43:22 +0200

    added field, post__init__ and save method

--- configs/configs.py
+++ configs/configs.py
@@ -1,41 +1,103 @@
-from dataclasses import dataclass
 
-##################################################
+import json
+from dataclasses import dataclass, field, asdict
+from typing import List, Dict
 
 @dataclass
 class DataConfig:
 
-    jet_type = 'tops'
-    features = ['eta_rel', 'phi_rel', 'log_pt_rel', 'log_e_rel', 'R']
-    preprocess = ['standardize']
-    datasets = {
-                'flow_midpoint'  : ['fm_tops150_cond_mp200nfe.h5', 'etaphipt'],
-                'diff_midpoint'  : ['midpoint_100_csts.h5', 'etaphipt_frac'],
-                'flow_euler'     : ['fm_tops150_cond_eu200nfe.h5', 'etaphipt'],
-                'diff_euler'     : ['euler_200_csts.h5', 'etaphipt_frac'],
-                'jetnet150'      : ['t150.hdf5', 'particle_features']
-                }
-    labels = {
-              'flow_midpoint'   : 0,
-              'diff_midpoint'   : 1,
-              'flow_euler'      : 2,
-              'diff_euler'      : 3,
-              'jetnet150'       : -1  # test dataset
-              } 
+    data_name : str = 'tops'
+    features   : List[str] = field(default_factory = lambda : ['eta_rel', 'phi_rel'])
+    preprocess : List[str] = field(default_factory = lambda : ['standardize'])
+    datasets   : Dict[str, List[str]] = field(default_factory = lambda: {'jetnet150': ['t150.hdf5', 'particle_features']})
+    labels : Dict[str, int] = field(default_factory = lambda:  {'jetnet150': -1} )
     
-##################################################
-
+    def __post_init__(self):
+        coords = ['eta_rel', 'phi_rel']
+        self.features = [feature for feature in coords if feature not in self.features] + self.features
+        testset = {'jetnet150': ['t150.hdf5', 'particle_features']}
+        testlabel = {'jetnet150': -1}
+        testset.update(self.datasets)
+        testlabel.update(self.labels)
+        self.datasets = testset
+        self.labels = testlabel
+    
 @dataclass
 class TrainConfig:
 
-    device = 'cuda:2'
-    split_fractions = [0.5, 0.2, 0.3]  # train / val / test 
-    size = None 
-    batch_size  = 1024
-    epochs = 10000   
-    early_stopping  = 30 
-    warmup_epochs = 300    
-    lr  = 0.001
-    seed = 12345
+    device : str = 'cpu'
+    split_fractions : List[float] = field(default_factory = lambda : [0.5, 0.2, 0.3])  # train / val / test 
+    size : int = None 
+    batch_size : int = 1024
+    epochs : int = 10000   
+    early_stopping : int = 30 
+    warmup_epochs : int = 100    
+    lr : float = 0.001
+    seed : int = 12345
 
-##################################################
+
+@dataclass
+class DeepSetsConfig(TrainConfig, DataConfig):
+
+    #...model parameters
+
+    model_name : str = 'DeepSets'
+    dim_input  : int = 2 
+    dim_output : int = 2
+    dim_hidden : int = 128   
+    num_layers_1 : int = 3
+    num_layers_2 : int = 3
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.dim_input = len(self.features)
+        self.dim_output = len(self.datasets) - 1
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            json.dump(asdict(self), f, indent=4)
+
+@dataclass
+class ParticleNetConfig(TrainConfig, DataConfig):
+
+    #...model parameters
+
+    model_name  : str = 'ParticleNet'
+    dim_input : int = 2
+    dim_output : int = 2
+    dim_hidden : int = 128 
+    num_knn : int = 7     
+    dim_conv_1 : int = 32
+    dim_conv_2 : int = 64
+    num_layers_1 : int = 3
+    num_layers_2 : int = 3
+    dropout : float = 0.1
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.dim_input = len(self.features)
+        self.dim_output = len(self.datasets) - 1
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            json.dump(asdict(self), f, indent=4)
+
+@dataclass
+class MLPConfig(TrainConfig, DataConfig):
+
+    #...model parameters
+
+    model_name : str = 'MLP'
+    dim_input : int = 5
+    dim_output : int = 2
+    dim_hidden : int = 128 
+    num_layers : int = 3 
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.dim_input = len(self.features)
+        self.dim_output = len(self.datasets) - 1
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            json.dump(asdict(self), f, indent=4)
--- configs/models.py
+++ configs/models.py
@@ -1,48 +1,41 @@
-from dataclasses import dataclass
-from configs.configs import DataConfig, TrainConfig
 
-##################################################
+# import sys
+# sys.path.append('/Users/dario/Dropbox/PROJECTS/ML/JetData/ClassifierMetric')
+
+from dataclasses import dataclass, field
+from configs import DataConfig, TrainConfig
 
 @dataclass
 class DeepSetsConfig(TrainConfig, DataConfig):
 
-    name = 'DeepSets'
-    dim_input = len(DataConfig.features)
-    dim_output = len(DataConfig.datasets) - 1
-    dim_hidden = 128   
-    num_layers_1 = 3
-    num_layers_2 = 3
-
-
-##################################################
-
+    model_name : str = 'DeepSets'
+    dim_input  : int = 5 #field(default_factory = lambda : len(DataConfig.features))
+    dim_output : int = 2 #field(default_factory = lambda : len(DataConfig.datasets) - 1)
+    dim_hidden : int = 128   
+    num_layers_1 : int = 3
+    num_layers_2 : int = 3
 
 @dataclass
 class ParticleNetConfig(TrainConfig, DataConfig):
 
-    name  = 'ParticleNet'
-    dim_input = len(DataConfig.features)
-    dim_output = len(DataConfig.datasets) - 1
-    dim_hidden = 128 
-    num_knn = 7     
-    dim_conv_1 = 32
-    dim_conv_2 = 64
-    num_layers_1 = 3
-    num_layers_2 = 3
-    dropout = 0.1
-
-
-##################################################
-
+    model_name  : str = 'ParticleNet'
+    dim_input : int = field(default_factory = lambda : len(DataConfig.features))
+    dim_output : int = field(default_factory = lambda : len(DataConfig.datasets) - 1)
+    dim_hidden : int = 128 
+    num_knn : int = 7     
+    dim_conv_1 : int = 32
+    dim_conv_2 : int = 64
+    num_layers_1 : int = 3
+    num_layers_2 : int = 3
+    dropout : float = 0.1
 
 @dataclass
 class MLPConfig(TrainConfig, DataConfig):
 
-    name = 'MLP'
-    dim_input = 5
-    dim_output = len(DataConfig.datasets) - 1
-    dim_hidden = 128 
-    num_layers = 3 
+    model_name : str = 'MLP'
+    dim_input : int = 5
+    dim_output : int = field(default_factory = lambda : len(DataConfig.datasets) - 1)
+    dim_hidden : int = 128 
+    num_layers : int = 3 
 
 
-##################################################
